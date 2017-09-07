7a5a1ea5b208e1b5621e10a7080f2af0023e7005
Anjesh Tuladhar <anjesh@yipl.com.np>
2017-09-07 11:10:58 +0545

    updated to ignore name which are very long, if the name already exists, updated data

--- src/cleanup/organisation.py
+++ src/cleanup/organisation.py
@@ -50,23 +50,48 @@ class OrganisationCollection:
         # add new organisation data to the collection
         self.addNewValidOrg(data)
 
+    def isCountryValid(self, countryname):
+        return countryname.strip() != "(No country assigned)"
+
+    """
+    checks whether the name of existing organisation is to be stored or not
+    """
+    def isOrgNameNew(self, data):
+        org = self.orgs[data[ORG.IDENTIFIER]]
+        newLanguage = True
+        for name in org[ORG.NAMELIST]:
+            if data[ORG.LANGUAGE] and name[ORG.LANGUAGE] == data[ORG.LANGUAGE]:
+                newLanguage = False
+            if name[ORG.NAME] == data[ORG.NAME]:
+                newLanguage = False
+        if newLanguage:
+            #new Language means this name is to be stored
+            return True
+        existingName = ""
+        for name in org[ORG.NAMELIST]:
+            if data[ORG.LANGUAGE] and name[ORG.LANGUAGE] == data[ORG.LANGUAGE]:
+                existingName = name[ORG.NAME]
+                break
+        if existingName.strip() == "":
+            return True
+        elif existingName == data[ORG.NAME]:
+            return False
+        else:
+            # if name already exists and the given name is long then 100 char, ignore.
+            if len(data[ORG.NAME])>100:
+                return False
+
     def updateOtherMetadata(self, data):
         org = self.orgs[data[ORG.IDENTIFIER]]
         if ORG.IS_PUBLISHER in data and data[ORG.IS_PUBLISHER]:
             org[ORG.IS_PUBLISHER] = data[ORG.IS_PUBLISHER]
         if ORG.IS_ORG_FILE in data and data[ORG.IS_ORG_FILE]:
             org[ORG.IS_ORG_FILE] = data[ORG.IS_ORG_FILE]
-        if ORG.COUNTRY in data and data[ORG.COUNTRY]:
+        if ORG.COUNTRY in data and data[ORG.COUNTRY] and self.isCountryValid(data[ORG.COUNTRY]):
             org[ORG.COUNTRY] = data[ORG.COUNTRY]
         if ORG.TYPE in data and data[ORG.TYPE]:
             org[ORG.TYPE] = data[ORG.TYPE]
-        newLanguage = True
-        for name in org[ORG.NAMELIST]:
-            if name[ORG.NAME] == data[ORG.NAME]:
-                newLanguage = False
-            if data[ORG.LANGUAGE] and name[ORG.LANGUAGE] == data[ORG.LANGUAGE]:
-                newLanguage == False
-        if newLanguage:
+        if self.isOrgNameNew(data):
             org[ORG.NAMELIST].append({
                 ORG.NAME: data[ORG.NAME],
                 ORG.LANGUAGE: self.getLanguage(data[ORG.LANGUAGE])
@@ -100,7 +125,7 @@ class OrganisationCollection:
             org[ORG.IS_PUBLISHER] = data[ORG.IS_PUBLISHER]
         if ORG.IS_ORG_FILE in data:
             org[ORG.IS_ORG_FILE] = data[ORG.IS_ORG_FILE]
-        if ORG.COUNTRY in data:
+        if ORG.COUNTRY in data and self.isCountryValid(data[ORG.COUNTRY]):
             org[ORG.COUNTRY] = data[ORG.COUNTRY]
         self.orgs[org[ORG.IDENTIFIER]] = org
         self.names[data[ORG.NAME]] = data[ORG.IDENTIFIER]
